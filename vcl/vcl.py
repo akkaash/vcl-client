b5623b7b95dd8c4d679b7806a25f72878fce4074
Akkaash Goel <agoel3@ncsu.edu>
2015-04-02 11:24:40 -0400

    added XMLRPCendRequest and other changes

--- vcl/vcl.py
+++ vcl/vcl.py
@@ -7,51 +7,63 @@ class Config(object):
         self.url = None
         self.username = None
         self.password = None
+        self.api = None
 
 pass_config = click.make_pass_decorator(Config, ensure=True)
 
-@click.group()
-@click.argument('url', type=click.STRING)
-@click.argument('username')
-@pass_config
-def cli(config, url, username):
+def make_config(config, url, username):
     config.url = url
     config.username = username
     config.password = getpass.getpass()
+    config.api = VCLApi(config.url, config.username, config.password)
 
+@click.group()
+@pass_config
+def cli(config):
+    pass
 
 @cli.command()
 @click.option('--string', help='string to send to VCL Site', default='Hello World!')
+@click.argument('url')
+@click.argument('username')
 @pass_config
-def test(config, string):
-    api = VCLApi(config.url, config.username, config.password)
-    response = api.test(string)
-    print response
-
-@cli.group()
-@pass_config
-def image(config):
-    pass
-
-@image.command()
-@pass_config
-def list(config):
-    api = VCLApi(config.url, config.username, config.password)
-    response = api.get_images()
+def test(config, string, url, username):
+    make_config(config, url, username)
+    response = config.api.test(string)
     click.echo(response)
 
-
-@cli.group()
+@cli.command()
+@click.option('--list', is_flag=True, flag_value=True, help='list available images')
+@click.argument('url')
+@click.argument('username')
 @pass_config
-def request(config):
-    pass
+def image(config, list, url, username):
+    make_config(config, url, username)
+    if list:
+        response = config.api.get_images()
+        click.echo(response)
 
-@request.command()
-@click.argument('image_id')
-@click.argument('start')
-@click.argument('length')
+@cli.command()
+@click.option('--add', help='add request', is_flag=True, flag_value=True)
+@click.option('--image-id', type=click.INT, help='image ID for request')
+@click.option('--start', help='unix timestamp for request start time')
+@click.option('--length', type=click.INT, help='length of request in 15 minute increments')
+@click.option('--end', help='end request', is_flag=True, flag_value=True)
+@click.option('--request-id', help='request ID', type=click.INT)
+@click.argument('url')
+@click.argument('username')
 @pass_config
-def add(config, image_id, start, length):
-    api = VCLApi(config.url, config.username, config.password)
-    response = api.add_request(image_id, start, length)
-    click.echo(response)
+def request(config, add, image_id, start, length,\
+        end, request_id, \
+        url, username):
+    make_config(config, url, username)
+    if add:
+        if start is None:
+            start = "now"
+        if length is None:
+            length = 15
+        response = config.api.add_request(image_id, start, length)
+        click.echo(response)
+    if end:
+        response = config.api.end_request(request_id)
+        click.echo(response)
--- vcl/vclapi.py
+++ vcl/vclapi.py
@@ -8,18 +8,21 @@ class VCLApi(object):
         self.username = username
         self.password = password
         self.verbose = 0
+        self.client = VCLServerProxy(self.url, self.username, self.password, verbose=0)
 
     def test(self, test_string):
-        client = VCLServerProxy(self.url, self.username, self.password, verbose=0)
-        rc = client.XMLRPCtest(test_string)
+        # client = VCLServerProxy(self.url, self.username, self.password, verbose=0)
+        rc = self.client.XMLRPCtest(test_string)
         return rc
 
     def get_images(self):
         client = VCLServerProxy(self.url, self.username, self.password, verbose=0)
-        rc = client.XMLRPCgetImages()
+        rc = self.client.XMLRPCgetImages()
         return rc
 
     def add_request(self, image_id, start, length):
-        client = VCLServerProxy(self.url, self.username, self.password, verbose=0)
-        rc = client.XMLRPCaddRequest(image_id, start, length)
+        rc = self.client.XMLRPCaddRequest(image_id, start, length)
         return rc
+
+    def end_request(self, request_id):
+        return self.client.XMLRPCendRequest(request_id)
