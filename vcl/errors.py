70f5ede8b4a0844744e0d64e2f1fda67d2446cb4
Akkaash Goel <agoel3@ncsu.edu>
2015-05-04 02:02:30 -0400

    add repr for VCLError

--- vcl/errors.py
+++ vcl/errors.py
@@ -1,4 +1,12 @@
 class VCLError(Exception):
     def __init__(self, message, error_code):
         super(VCLError, self).__init__(message)
-        self.error_code = error_code
+        self._error_code = error_code
+
+    @property
+    def error_code(self):
+        return self._error_code
+
+    def __repr__(self):
+        return "%s(error_code=%r, message=%r)" % (self.__class__, self.error_code,
+                                                  self.message)
